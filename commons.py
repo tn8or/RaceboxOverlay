f33f408410aa7d151314288b5801d4caad84b8f4
Tommy Eriksen <tommy.eriksen@gmail.com>
2024-06-10 18:14:44 +0200

    and don't mangle tmpdir name

--- commons.py
+++ commons.py
@@ -53,11 +53,7 @@ class dashGenerator:
         self.filename = filename
         self.rows = rows
         self.header = header
-        self.foldername = (
-            self.tmpdir.name
-            + hashlib.md5(json.dumps(rows, sort_keys=True).encode("utf-8")).hexdigest()
-            + "/"
-        )
+        self.foldername = self.tmpdir.name + "/"
         self.log = []
 
         if width == 1920:
